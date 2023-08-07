d626090b5c8c055a20fdceaed3938cb79090e518
Hernan Zamora <hernanzamora1999@gmail.com>
2023-08-07 17:57:03 -0300

    Created food scaffold - Created food table - User can now add new food and delete food inputted in the table - Styles needed

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,43 @@
+class FoodsController < ApplicationController
+  before_action :set_food, only: %i[ show edit update destroy ]
+
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = current_user.id
+
+    if @food.save
+      flash[:notice] = "Food was successfully added to the table."
+      redirect_to foods_path
+    else
+      flash[:alert] = "Food was not added to the table."
+      render :new
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    puts params[:id]
+    @food.destroy
+    flash[:notice] = 'Food was successfully deleted.'
+    redirect_to foods_path
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_food
+      @food = Food.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def food_params
+      params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+    end
+end
